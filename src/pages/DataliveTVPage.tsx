c81343bcb130337db5628d55f48cd189c39f6953
ramirolacci <ramiroalejandrolacci19@gmail.com>
2026-05-13 10:43:01 -0300

    correcion de icono en boton

--- src/pages/DataliveTVPage.tsx
+++ src/pages/DataliveTVPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Plus, Search, Monitor, User, CreditCard as Edit, Trash2, Save, LayoutGrid, Copy, Check, Key } from "lucide-react";
+import { Plus, Search, Monitor, User, Pencil as Edit, Trash2, Save, LayoutGrid, Copy, Check, Key } from "lucide-react";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
